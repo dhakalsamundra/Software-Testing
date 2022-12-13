201b72c0e4584f2b1616b59f28dcc8c37da50496
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-12-13 18:17:19 +0200

    add test for countBy function

---
+++ test/countBy.test.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import countBy from "../src/countBy.js";
+
+describe("Function of Counting via a arg", () => {
+
+    const invalidFunction = "Iphone 14";
+    const users = [
+        { 'user': 'barney', 'active': true },
+        { 'user': 'betty', 'active': true },
+        { 'user': 'fred', 'active': false }
+       ]
+
+
+     it("Count the array of object with an invalid predicate throws error",  () => {
+        expect(function(){ countBy(users, invalidFunction) }).to.throw(TypeError);
+    });
+  
+    it("Count the array of object without predicate throws error",  () => {
+        expect(function(){ countBy(users, invalidFunction) }).to.throw(TypeError);
+    });
+
+    it("count the number of active users", () => {
+            let result = countBy(users, (each) => each.active);
+            expect(result).to.deep.eq({ 'true': 2, 'false': 1 });
+    })
+});
