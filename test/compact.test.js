feabfadf6bf7df92fd5cf1096515b1f39299fd8f
Samundra Dhakal <samundra.dhakal@tuni.fi>
2022-12-15 19:44:09 +0200

    uncomment

--- test/compact.test.js
+++ test/compact.test.js
@@ -2,32 +2,27 @@ import { expect } from 'chai';
 import compact from '../src/compact.js';
 
 describe('Creates an array with all falsey values removed', () => {
-  //   it('Return empty array if pass an empty array', () => {
-  //     let result = compact([]);
-  //     expect(result).to.deep.eq([]);
-  //   });
+    it('Return empty array if pass an empty array', () => {
+      let result = compact([]);
+      expect(result).to.deep.eq([]);
+    });
 
-  //   it("Throw error message as 'Array is not iterable' if pass undefined", () => {
-  //     let callFunction = () => compact(undefined);
-  //     expect(callFunction).to.throw('array is not iterable');
-  //   });
+    it("Throw error message as 'Array is not iterable' if pass undefined", () => {
+      let callFunction = () => compact(undefined);
+      expect(callFunction).to.throw('array is not iterable');
+    });
 
-  //   it('remove the falsy value from the array', () => {
-  //     let result = compact([0, 1, false, 2, 3]);
-  //     expect(result).to.deep.eq([1, 2, 3]);
-  //   });
+    it('remove the falsy value from the array', () => {
+      let result = compact([0, 1, false, 2, 3]);
+      expect(result).to.deep.eq([1, 2, 3]);
+    });
 
-  //   it('remove the falsy value from the array', () => {
-  //     expect(
-  //       compact([0, '-1', 1, false, undefined, 2, '', 'product2'])
-  //     ).to.deep.equal([1, 2, 'product2']);
-  //     expect(compact([0, '-1', 1, false, undefined, 2, '', 'product2'])).to.be.an(
-  //       'array'
-  //     );
-  //   });
-
-  it('remove the falsy value from the array', () => {
-    expect(compact([0, '-1', 1, false, 2, '', 3])).to.deep.equal([1, 2, 3]);
-    expect(compact([0, '-1', 1, false, 2, '', 3])).to.be.an('array');
-  });
+    it('remove the falsy value from the array', () => {
+      expect(
+        compact([0, 1, false, undefined, 2, '', 'product2'])
+      ).to.deep.equal([1, 2, 'product2']);
+      expect(compact([0, 1, false, undefined, 2, '', 'product2'])).to.be.an(
+        'array'
+      );
+    });
 });
